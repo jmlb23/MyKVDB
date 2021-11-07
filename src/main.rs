5de29e7a0d65d32d352104d197e9137083ff1eec
jmlb23 <rmmenosrf@riseup.net>
2021-11-07 20:51:08 +0100

    feat: added prototype cli options

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,30 @@
 use api::api::Api;
-
+use std::env;
 fn main() {
-    let api = Api::new("127.0.0.1:8080");
+    let vector = env::args().collect::<Vec<String>>();
+    let args = vector.iter().map(|x| x.as_str()).collect::<Vec<&str>>();
+    let (_, tail) = args.split_at(1);
+    match tail {
+        [] => run_server("127.0.0.1", "8080"),
+        ["--help", ..] => print_help(),
+        ["--port", port] => run_server("127.0.0.1", port),
+        ["--host", host] => run_server(host, "8080"),
+        ["--host", host, "--port", port, ..] | ["--port", port, "--host", host, ..] => {
+            run_server(host, port)
+        }
+        _ => {
+            println!("KVDB: unrecognized option `{}`", tail.join(" "));
+            print_help();
+        }
+    };
+}
+
+fn run_server(host: &str, port: &str) {
+    let api = Api::new(format!("{}:{}", host, port).as_str());
+    println!("server running at {}:{}", host, port);
     api.poll();
+}
+
+fn print_help() {
+    print!("usage: MKDB [--port NUMBER] [--host HOST]")
 }
