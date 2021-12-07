34ff4cf001b42237688747902aa373878ee38341
jmlb23 <rmmenosrf@riseup.net>
2021-12-08 00:05:32 +0100

    fix: changed to Mutex and added crossbeam

--- src/lib/store_api.rs
+++ src/lib/store_api.rs
@@ -1,27 +1,27 @@
-use std::cell::RefCell;
+use std::sync::Mutex;
 use std::sync::Arc;
 use std::collections::BTreeMap;
 
 pub struct Store {
-    store: Arc<RefCell<BTreeMap<String, String>>>,
+    store: Arc<Mutex<BTreeMap<String, String>>>,
 }
 
 impl Store {
     pub fn new() -> Self {
         Store {
-            store: Arc::new(RefCell::new(BTreeMap::new())),
+            store: Arc::new(Mutex::new(BTreeMap::new())),
         }
     }
 
     pub fn insert(&self, key: &String, value: &String) {
-        &self.store.borrow_mut().insert(key.to_owned(), value.to_owned());
+        &self.store.try_lock().expect("Can't obtain a lock").insert(key.to_owned(), value.to_owned());
     }
 
     pub fn remove(&self, key: &String) {
-        &self.store.borrow_mut().remove(key);
+        &self.store.try_lock().expect("Can't obtain a lock").remove(key);
     }
 
     pub fn get(&self, key: &String) -> Option<String> {
-        self.store.borrow().get(key).map(|x| x.to_owned())
+        self.store.try_lock().expect("Can't obtain a lock").get(key).map(|x| x.to_owned())
     }
 }
