d87cbba39f42b5d095a7bb5188e540cf8e21f794
jmlb23 <rmmenosrf@riseup.net>
2021-12-08 20:04:08 +0100

    format: use rust formater

--- src/lib/api.rs
+++ src/lib/api.rs
@@ -1,35 +1,41 @@
-use std::net::TcpListener;
+use crate::store_api::Store;
 use std::io::BufRead;
 use std::io::BufReader;
 use std::io::BufWriter;
-use std::net::TcpStream;
 use std::io::Write;
-use crate::store_api::Store;
-enum Command{
-   Ping, Set(String, String), Delete(String), Get(String), Update(String, String), CommandNotRecognized
+use std::net::TcpListener;
+use std::net::TcpStream;
+enum Command {
+    Ping,
+    Set(String, String),
+    Delete(String),
+    Get(String),
+    Update(String, String),
+    CommandNotRecognized,
 }
 
 pub struct Api {
     socket: TcpListener,
-    store: Store
+    store: Store,
 }
 
 impl Api {
-
     pub fn new(address: &str) -> Self {
         Api {
-          socket: TcpListener::bind(address).unwrap(),
-          store: Store::new()
+            socket: TcpListener::bind(address).unwrap(),
+            store: Store::new(),
         }
     }
-    
     fn parse(message: &str) -> Command {
         println!("{}", message);
         if message.contains("PING") {
             Command::Ping
         } else if message.contains("SET") {
             let vector = message.split_whitespace().collect::<Vec<&str>>();
-            Command::Set(vector[1].to_owned().to_string(),vector[2].to_owned().to_string())
+            Command::Set(
+                vector[1].to_owned().to_string(),
+                vector[2].to_owned().to_string(),
+            )
         } else if message.contains("DELETE") {
             let vector = message.split_whitespace().collect::<Vec<&str>>();
             Command::Delete(vector[1].to_owned().to_string())
@@ -38,59 +44,60 @@ impl Api {
             Command::Get(vector[1].to_owned().to_string())
         } else if message.contains("UPDATE") {
             let vector = message.split_whitespace().collect::<Vec<&str>>();
-            Command::Update(vector[1].to_owned().to_string(),vector[2].to_owned().to_string())
+            Command::Update(
+                vector[1].to_owned().to_string(),
+                vector[2].to_owned().to_string(),
+            )
         } else {
             Command::CommandNotRecognized
         }
     }
 
-    fn handle(&self, writer: &mut BufWriter<&TcpStream>, message: &str){
-        let command = Api::parse(&message.replace("\n",""));
+    fn handle(&self, writer: &mut BufWriter<&TcpStream>, message: &str) {
+        let command = Api::parse(&message.replace("\n", ""));
         match command {
             Command::Ping => {
                 writer.write("PONG".as_bytes()).unwrap();
-            },
-            Command::Set(key, value) => {
-                self.store.insert(&key, &value)
-            },
+            }
+            Command::Set(key, value) => self.store.insert(&key, &value),
             Command::Delete(key) => {
                 self.store.remove(&key);
                 writer.write(key.as_bytes()).unwrap();
-            },
+            }
             Command::Get(key) => {
-                if let Some(value) = &self.store.get(&key){
+                if let Some(value) = &self.store.get(&key) {
                     writer.write(value.as_bytes()).expect("Unexpected error.");
                 } else {
                     writer.write("0".as_bytes()).expect("Unexpected error.");
                 }
-            },
+            }
             Command::Update(key, new_value) => {
                 self.store.remove(&key);
                 self.store.insert(&key, &new_value);
-            },
+            }
             _ => {
                 writer.write("Not Recognized".as_bytes()).unwrap();
-            },
+            }
         }
     }
-    
 
-    pub fn poll(&self){
-      for stream in self.socket.incoming() {
-        match stream {
-            Ok(stream) => {
-                let mut reader = BufReader::new(&stream);
-                let mut writer = BufWriter::new(&stream);
-                let mut buffer = Vec::new();
-                let _ = reader.read_until(b'\n', &mut buffer);
-                let req_as_string = String::from_utf8(buffer).expect("Error parsing request").to_owned();
-                self.handle(&mut writer, &req_as_string);
-            }
-            Err(_) => { 
-                println!("Unexpected Error")
+    pub fn poll(&self) {
+        for stream in self.socket.incoming() {
+            match stream {
+                Ok(stream) => {
+                    let mut reader = BufReader::new(&stream);
+                    let mut writer = BufWriter::new(&stream);
+                    let mut buffer = Vec::new();
+                    let _ = reader.read_until(b'\n', &mut buffer);
+                    let req_as_string = String::from_utf8(buffer)
+                        .expect("Error parsing request")
+                        .to_owned();
+                    self.handle(&mut writer, &req_as_string);
+                }
+                Err(_) => {
+                    println!("Unexpected Error")
+                }
             }
         }
-      }
     }
 }
-
--- src/lib/store_api.rs
+++ src/lib/store_api.rs
@@ -1,6 +1,6 @@
-use std::sync::Mutex;
-use std::sync::Arc;
 use std::collections::BTreeMap;
+use std::sync::Arc;
+use std::sync::Mutex;
 
 pub struct Store {
     store: Arc<Mutex<BTreeMap<String, String>>>,
@@ -14,14 +14,26 @@ impl Store {
     }
 
     pub fn insert(&self, key: &String, value: &String) {
-        &self.store.try_lock().expect("Can't obtain a lock").insert(key.to_owned(), value.to_owned());
+        &self
+            .store
+            .try_lock()
+            .expect("Can't obtain a lock")
+            .insert(key.to_owned(), value.to_owned());
     }
 
     pub fn remove(&self, key: &String) {
-        &self.store.try_lock().expect("Can't obtain a lock").remove(key);
+        &self
+            .store
+            .try_lock()
+            .expect("Can't obtain a lock")
+            .remove(key);
     }
 
     pub fn get(&self, key: &String) -> Option<String> {
-        self.store.try_lock().expect("Can't obtain a lock").get(key).map(|x| x.to_owned())
+        self.store
+            .try_lock()
+            .expect("Can't obtain a lock")
+            .get(key)
+            .map(|x| x.to_owned())
     }
 }
